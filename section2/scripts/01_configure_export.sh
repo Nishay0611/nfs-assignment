cb32414f0507c8162d4af4f7aa75554a0eb06d27
Nishay0611 <shayni611@gmail.com>
2026-02-14 20:02:19 +0200

    fix script

--- section2/scripts/01_configure_export.sh
+++ section2/scripts/01_configure_export.sh
@@ -13,28 +13,13 @@ echo "Alcor IP: $ALCOR_IP"
 
 # 3) Update /etc/exports (remove any old section2 line, then add the restricted one)
 docker exec Mizar bash -c "sed -i '/\/srv\/nfs\/share\/section2/d' /etc/exports"
-docker exec Mizar bash -c "echo '/srv/nfs/share/section2 ${ALCOR_IP}(rw,sync,no_subtree_check,root_squash)' >> /etc/exports
-#!/usr/bin/env bash
-set -e
-
-echo "Section 2: Configure NFS export restricted to Alcor + validate mount"
-
-# 1) Create directory for Section 2 on Mizar
-docker exec Mizar mkdir -p /srv/nfs/share/section2
-docker exec Mizar chmod 777 /srv/nfs/share/section2
-
-# 2) Get Alcor IP
-ALCOR_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' Alcor)
-echo "Alcor IP: $ALCOR_IP"
-
-# 3) Update /etc/exports (remove any old section2 line, then add the restricted one)
-docker exec Mizar bash -c "sed -i '/\/srv\/nfs\/share\/section2/d' /etc/exports"
 docker exec Mizar bash -c "echo '/srv/nfs/share/section2 ${ALCOR_IP}(rw,sync,no_subtree_check,root_squash)' >> /etc/exports"
+
 # 4) Reload exports and show active exports
 docker exec Mizar exportfs -rav
 docker exec Mizar exportfs -v
 
-echo "Validating that Alcor can mount and access the share..."
+echo "Validating that Alcor can mount and access the share"
 
 # 5) Prepare mount point on Alcor
 docker exec Alcor mkdir -p /mnt/section2
@@ -54,4 +39,3 @@ docker exec Mizar ls -l /srv/nfs/share/section2/alcor_test.txt
 docker exec Mizar cat /srv/nfs/share/section2/alcor_test.txt
 
 echo "DONE: Export configured + Alcor mount/read/write validated."
-
